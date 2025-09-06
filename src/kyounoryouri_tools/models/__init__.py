c1b53f28431f8a48128161196032263ded9c533c
Mashiro Toyooka <92073972+mashi6n@users.noreply.github.com>
2025-09-06 19:28:17 +0900

    fix: export Recipe

--- src/kyounoryouri_tools/models/__init__.py
+++ src/kyounoryouri_tools/models/__init__.py
@@ -1,4 +1,5 @@
 from .raw_recipe import Ingredient, Nutrient, RawRecipe, Step, TitledStep
+from .recipe import Recipe
 from .urlset import Url, Urlset
 
-__all__ = ["Ingredient", "Nutrient", "RawRecipe", "Step", "TitledStep", "Url", "Urlset"]
+__all__ = ["Ingredient", "Nutrient", "RawRecipe", "Recipe", "Step", "TitledStep", "Url", "Urlset"]
